234534e4375dc0c11d01c65180a136ecc82c7fc7
Silvestre <Silvestre@LAPTOP-2R90FK4G>
2019-03-07 00:26:39 -0400

    Test de CuentaBancaria y Deposito.

---
+++ SegundoParcialWF.Tests/CuentaBancariaUT.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using BLL;
+using Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SegundoParcialWF.Tests
+{
+    [TestClass]
+    public class CuentaBancariaUT
+    {
+        [TestMethod]
+        public void Guardar()
+        {
+            Repositorio<CuentaBancaria> repositorio = new Repositorio<CuentaBancaria>();
+            CuentaBancaria cuenta = new CuentaBancaria();
+            bool paso = false;
+
+            cuenta.CuentaBancariaId = 4;
+            cuenta.Fecha = DateTime.Now;
+            cuenta.Nombre = "Juan";
+            cuenta.Balance = 0;
+
+            paso = repositorio.Guardar(cuenta);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod]
+        public void Modificar()
+        {
+            var cuenta = BuscarM();
+            Repositorio<CuentaBancaria> repositorio = new Repositorio<CuentaBancaria>();
+
+            bool paso = false;
+            cuenta.Nombre = "Alfredo";
+            paso = repositorio.Modificar(cuenta);
+            Assert.AreEqual(true, paso);
+        }
+
+        public CuentaBancaria BuscarM()
+        {
+            int id = 3;
+            Repositorio<CuentaBancaria> repositorio = new Repositorio<CuentaBancaria>();
+            CuentaBancaria cuenta = new CuentaBancaria();
+            cuenta = repositorio.Buscar(id);
+            return cuenta;
+        }
+
+        [TestMethod]
+        public void Eliminar()
+        {
+            Repositorio<CuentaBancaria> repositorio = new Repositorio<CuentaBancaria>();
+            int id = 4;
+            bool paso = false;
+            paso = repositorio.Eliminar(id);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod]
+        public void Buscar()
+        {
+            int id = 3;
+            Repositorio<CuentaBancaria> repositorio = new Repositorio<CuentaBancaria>();
+            CuentaBancaria cuenta = new CuentaBancaria();
+            cuenta = repositorio.Buscar(id);
+            Assert.IsNotNull(cuenta);
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            Repositorio<CuentaBancaria> repositorio = new Repositorio<CuentaBancaria>();
+            List<CuentaBancaria> lista = new List<CuentaBancaria>();
+            Expression<Func<CuentaBancaria, bool>> resultados = p => true;
+            lista = repositorio.GetList(resultados);
+            Assert.IsNotNull(lista);
+        }
+    }
+}
---
+++ SegundoParcialWF.Tests/DepositoUT.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using BLL;
+using Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SegundoParcialWF.Tests
+{
+    [TestClass]
+    public class DepositoUT
+    {
+        [TestMethod]
+        public void Guardar()
+        {
+            RepositoDeposito repositorio = new RepositoDeposito();
+            Deposito deposito = new Deposito();
+            bool paso = false;
+
+            deposito.DepositoId = 3;
+            deposito.Fecha = DateTime.Now;
+            deposito.CuentaId = 3;
+            deposito.Concepto = "Pago de Lenington";
+            deposito.Monto = 200;
+
+            paso = repositorio.Guardar(deposito);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod]
+        public void Modificar()
+        {
+            var deposito = BuscarM();
+            RepositoDeposito repositorio = new RepositoDeposito();
+
+            bool paso = false;
+            deposito.Concepto = "Pago de Alfredo";
+            paso = repositorio.Modificar(deposito);
+            Assert.AreEqual(true, paso);
+        }
+
+        public Deposito BuscarM()
+        {
+            int id = 2;
+            RepositoDeposito repositorio = new RepositoDeposito();
+            Deposito deposito = new Deposito();
+            deposito = repositorio.Buscar(id);
+            return deposito;
+        }
+
+        [TestMethod]
+        public void Eliminar()
+        {
+            RepositoDeposito repositorio = new RepositoDeposito();
+            int id = 3;
+            bool paso = false;
+            paso = repositorio.Eliminar(id);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod]
+        public void Buscar()
+        {
+            int id = 1;
+            Repositorio<Deposito> repositorio = new Repositorio<Deposito>();
+            Deposito deposito = new Deposito();
+            deposito = repositorio.Buscar(id);
+            Assert.IsNotNull(deposito);
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            Repositorio<Deposito> repositorio = new Repositorio<Deposito>();
+            List<Deposito> lista = new List<Deposito>();
+            Expression<Func<Deposito, bool>> resultados = p => true;
+            lista = repositorio.GetList(resultados);
+            Assert.IsNotNull(lista);
+        }
+    }
+}
