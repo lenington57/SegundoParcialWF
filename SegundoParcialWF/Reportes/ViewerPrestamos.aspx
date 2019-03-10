b2c381a603a1069edec2106acb6b3f144a1a2dad
Silvestre <Silvestre@LAPTOP-2R90FK4G>
2019-03-10 00:45:05 -0400

    Programando PrestamosReporte.

---
+++ SegundoParcialWF/Reportes/ViewerPrestamos.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewerPrestamos.aspx.cs" Inherits="SegundoParcialWF.Reportes.ViewerPrestamos" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=15.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="div1">
+            <%--ScriptManager--%>
+            <asp:ScriptManager runat="server"></asp:ScriptManager> 
+            
+            <%--Viewer--%>
+            <rsweb:ReportViewer ID="MyPrestamosReportViewer" runat="server" ProcessingMode="Remote" Height="718px" Width="1000px">
+                <ServerReport ReportPath="" ReportServerUrl="" />
+            </rsweb:ReportViewer>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SegundoParcialWF/Reportes/ViewerPrestamos.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SegundoParcialWF.Reportes
+{
+    public partial class ViewerPrestamos : System.Web.UI.Page
+    {
+        Expression<Func<Prestamo, bool>> filtro = p => true;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                MyPrestamosReportViewer.ProcessingMode = ProcessingMode.Local;
+                MyPrestamosReportViewer.Reset();
+                MyPrestamosReportViewer.LocalReport.ReportPath = Server.MapPath(@"~\Reportes\ListadoPrestamos.rdlc");
+                MyPrestamosReportViewer.LocalReport.DataSources.Clear();
+                MyPrestamosReportViewer.LocalReport.DataSources.Add(new ReportDataSource("PrestamosDS", BLL.Metodos.FPrestamos(filtro)));
+                MyPrestamosReportViewer.LocalReport.Refresh();
+            }
+        }
+    }
+}
